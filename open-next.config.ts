b8abb124eb15a917fd5e9f84c58500adc3e270ee
Saksham Dubey <dubeysaksham801@gmail.com>
2026-03-20 14:18:43 +0530

    cloudflare fix bug dependices add 6

--- open-next.config.ts
+++ open-next.config.ts
@@ -3,7 +3,9 @@ const config = {
     override: {
       wrapper: "cloudflare-node",
       converter: "edge",
-      proxyExternalRequest: "fetch",
+      incrementalCache: "dummy",
+      tagCache: "dummy",
+      queue: "dummy",
     },
   },
 };
